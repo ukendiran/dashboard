dfc497f4d5d68c92aedee301a6c2c3098048e999
Ukendiran <ukeendiran@gmail.com>
2024-02-21 22:08:14 +0530

    changes

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,26 +1,62 @@
-// pages/Dashboard.js
-import React from 'react';
 
-import MasterLayout from '../components/Layout';
-const Dashboard = () => {
+import React from 'react';
+import MenuItem from '@mui/material/MenuItem';
+import BarChart from '../charts/BarChart';
+import BarChart1 from '../charts/BarChart1';
+import PieChart from '../charts/PieChart';
+import PieChart1 from '../charts/PieChart1';
+import PieChart2 from '../charts/PieChart2';
+import Select from '@mui/material/Select';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+
+export const MainPage = () => {
+
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+
+
     return (
         <div className="dashboard">
-            <Sidebar />
+            <div className="row">
+                <div className='col-md-6'></div>
+                <div className='col-md-6 d-flex justify-content-end'>
+                    <FormControl sx={{ m: 1, minWidth: 250 }}>
+                        <InputLabel id="demo-simple-select-helper-label">Country</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-helper-label"
+                            id="demo-simple-select-helper"
+                            value={age}
+                            label="Select Country"
+                            onChange={handleChange}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={'Germany'}>Germany</MenuItem>
+                            <MenuItem value={'France'}>France</MenuItem>
+                            <MenuItem value={'India'}>India</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+            </div>
             <div className="container">
-                <Header />
+
                 <div className='row'>
                     <div className='col-md-6'>
-                        <div className='card shadow p-3 bg-white rounded'>
+                        <div className='card'>
                             <div className='card-body'>
-                            <h5 class="card-title">Discrepancies</h5>
                                 <BarChart />
                             </div>
                         </div>
                     </div>
                     <div className='col-md-6'>
-                        <div className='card shadow p-3 bg-white rounded'>
+                        <div className='card'>
                             <div className='card-body'>
-                                <h5 class="card-title">Discrepancies</h5>
                                 <BarChart1 />
                             </div>
                         </div>
@@ -28,12 +64,10 @@ const Dashboard = () => {
                 </div>
                 <div className='row'>
                     <div className='col-md-6 mt-5'>
-                        <div className='card shadow p-3 bg-white rounded'>
+                        <div className='card'>
                             <div className='card-body'>
-                            <h5 class="card-title">Discrepancies</h5>
                                 <div className='row'>
                                     <div className='col-md-6 mt-5'>
-                                        
                                         <PieChart />
                                     </div>
                                     <div className='col-md-6 mt-5'>
@@ -52,9 +86,8 @@ const Dashboard = () => {
                         </div>
                     </div>
                     <div className='col-md-6 mt-5'>
-                        <div className='card shadow p-3 bg-white rounded'>
+                        <div className='card'>
                             <div className='card-body'>
-                            <h5 class="card-title">Discrepancies</h5>
                                 <div className='row'>
                                     <div className='col-md-6 mt-5'>
                                         <div className='card'>
@@ -70,7 +103,7 @@ const Dashboard = () => {
                                         <div className='card'>
                                             <div className='card'>
                                                 <div className='card-body text-center'>
-                                                    <h5>Cost Reference ID</h5>
+                                                    <h5>WD_COSTCENTER_ID</h5>
                                                     <p>FR</p>
                                                 </div>
                                             </div>
@@ -82,7 +115,7 @@ const Dashboard = () => {
                                         <div className='card'>
                                             <div className='card'>
                                                 <div className='card-body text-center'>
-                                                    <h5>Position Reference ID</h5>
+                                                    <h5>WD_POSITIONID</h5>
                                                     <p>CH</p>
                                                 </div>
                                             </div>
@@ -92,7 +125,7 @@ const Dashboard = () => {
                                         <div className='card'>
                                             <div className='card'>
                                                 <div className='card-body text-center'>
-                                                    <h5>Pay Group Reference ID</h5>
+                                                    <h5>WD_PAYGROUP_ID</h5>
                                                     <p>SA</p>
                                                 </div>
                                             </div>
@@ -105,7 +138,5 @@ const Dashboard = () => {
                 </div>
             </div>
         </div>
-    );
-};
-
-export default Dashboard;
+    )
+}
